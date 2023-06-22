5e6d3f5cf14508636ace9849dd0ad101422eb4ae
joshua <joshk.5793@gmail.com>
2023-06-22 15:08:49 -0400

    finished sum

---
+++ practice-for-ch-js-argument-exercises-long-practice-main/argument.js
@@ -0,0 +1,20 @@
+// function sum() {
+//     let sum = 0;
+
+//     for (let i = 0; i < arguments.length; i++) {
+//         sum += arguments[i];
+//     };
+//     return sum;
+// };
+
+// function sum(...args) {
+//     let sum = 0
+
+//     args.forEach(arg => {
+//         sum += arg
+//     })
+//     return sum
+// }
+
+// console.log(sum(1, 2, 3, 4) === 10)
+// console.log(sum(1, 2, 3, 4, 5) === 15)
